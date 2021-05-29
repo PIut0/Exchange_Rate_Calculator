5bea5a3c88d4a38a9a59e02535ee5de5b01d26c5
Plut0 <slimdoks@gmail.com>
2021-05-30 01:11:57 +0900

    feat: add initializing currency selectBox

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
 import { } from "./get_exchange.js";
 import { getCurrencyData } from "./parseCurrencyData.js";
 
+let addSelectBox = (currencyData, selectId, defaultCurrency) => {
+	let selectBox = document.querySelector(selectId);
+	console.log(selectBox);
+	for (let i = 0; i < currencyData.length; i++) {
+		let element = document.createElement("option");
+		let text = currencyData[i].code + " (" + currencyData[i].symbol + ")";
+		let textNode = document.createTextNode(text);
+		element.appendChild(textNode);
+		if (currencyData[i].code === defaultCurrency)
+			element.selected = true;
+		element.value = currencyData[i].code;
+		selectBox.appendChild(element);
+	}
+}
+
+let initSelectData = async () => {
+	let currencyData = await getCurrencyData();
+	addSelectBox(currencyData, '#from_country', 'USD');
+	addSelectBox(currencyData, '#to_country', 'KRW');
+}
+
 window.onload = async () => {
-
-	let data = await getCurrencyData();
-
-	console.log(data);
+	initSelectData();
 }
